bdc9d46d28f2135405bcf2c38eeb2ab8a241df7b
지현 <57143818+spy03128@users.noreply.github.com>
2020-12-23 01:35:43 +0900

    Make sayhello function

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log('Im Working. Im JS. Im Beautiful. Im worth it')
+
+// const a = 221; //수정불가능, 기본적으로 사용
+// let b = a-5;
+// a = 4;
+// console.log(b, a);
+
+// const what = "jihyeon";
+// console.log(what);
+
+function sayhello(name, age){
+    // console.log('Hello!', name," You have ",age," years of age.");
+    console.log(`Hello ${name} you are ${age} years old.`);
+}
+
+sayhello("Nicolas",15);
+console.log()
