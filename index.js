6686ae83fea18c71a633aec5cc8627b360ae378f
지현 <57143818+spy03128@users.noreply.github.com>
2020-12-28 00:59:37 +0900

    DOM finish

--- index.js
+++ index.js
@@ -16,9 +16,77 @@
 // sayhello("Nicolas",15);
 // console.log()
 
-const title = document.getElementById("title")
+// const title = document.getElementById("title")
 
-console.dir(title);
-console.error("Fuck");
-title.innerHTML = "Hi From JS";
-title.style.color = 'white';
+// console.dir(title);
+// console.error("Fuck");
+// title.innerHTML = "Hi From JS";
+// title.style.color = 'white';
+
+// const title = document.querySelector("#title");
+
+// const BASE_COLOR = "rgb(52, 73, 94)";
+// const OTHER_COLOR = "#D6A2E8";
+
+// function handleClick() {
+//   // console.log("I have been resized");
+//   // console.log(title.style.color);
+//   const currentColor = title.style.color;
+//   if (currentColor === BASE_COLOR) {
+//     title.style.color = OTHER_COLOR;
+//   } else {
+//     title.style.color = BASE_COLOR;
+//   }
+// }
+
+// // window.addEventListener("resize", handleResize); //handleResize() 이렇게하면 바로 적용되는것
+
+// function init() {
+//   title.style.color = BASE_COLOR;
+//   title.addEventListener("click", handleClick);
+// }
+
+// init();
+
+// const age = prompt("How old are you");
+
+// console.log(age);
+
+// if (age >= 18 && age <= 21) {
+//   console.log("You can drink but you should not");
+// } else if (age > 21) {
+//   console.log("go ahed");
+// } else {
+//   console.log("too young");
+// }
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  //   const currentClass = title.className;
+
+  //1번으로 사용하기
+  //   const hasClass = title.classList.contains(CLICKED_CLASS);
+  //   if (hasClass) {
+  //     title.classList.remove(CLICKED_CLASS);
+  //   } else {
+  //     title.classList.add(CLICKED_CLASS);
+  //   }
+
+  //2번으로 사용하기 toggle
+  title.classList.toggle(CLICKED_CLASS);
+}
+//   if (currentClass !== CLICKED_CLASS) {
+//     title.classList.add(CLICKED_CLASS);
+//   } else {
+//     title.classList.remove(CLICKED_CLASS);
+//   }
+// }
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+
+init();
